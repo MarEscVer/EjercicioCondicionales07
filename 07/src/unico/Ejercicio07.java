3a0b69a4259fd584a36a30b09c8abed8d972b19a
Maria <maria_escribanoverde@gmail.com>
2022-11-07 16:05:02 +0100

    Final-proyecto

---
+++ 07/src/unico/Ejercicio07.java
@@ -0,0 +1,44 @@
+package unico;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		final int INS = 5;
+		final int SUF = 6;
+		final int BIEN = 7;
+		final int NOT = 9;
+		
+		double nota;
+		String boletin;
+
+		
+		System.out.print("Introduzca la nota para conocer su valor en el boletín: ");
+		nota = Double.parseDouble(teclado.nextLine());
+		
+		if (nota < INS) {
+			boletin = "Insuficiente";
+		} else {
+			if (nota < SUF) {
+				boletin = "Suficiente";
+			} else {
+				if (nota < BIEN) {
+					boletin = "Bien";
+				} else {
+					if (nota < NOT) {
+						boletin = "Notable";
+					} else {
+						boletin = "Sobresaliente";
+					}
+				}
+			}
+		}
+		
+		System.out.println(boletin);
+		
+	}
+		
+}
